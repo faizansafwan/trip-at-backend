70a62c25b3697c5bbb95fb31bdfc82c3036be608
faizansafwan <faizansafwan075@gmail.com>
2024-10-31 14:56:50 +0530

    added basic packages, create server and integrate with mongodb database

---
+++ server.js
@@ -0,0 +1,32 @@
+
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import User from "./routes/User.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use('/api/user', User);
+
+app.get( '/', (req,res) => {
+    res.send("Check get request");
+});
+
+dotenv.config();
+const uri = process.env.ATLAS_URL;
+mongoose.connect( uri, {});
+
+const connection = mongoose.connection;
+connection.once( "open", () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
