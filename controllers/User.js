bcddc926e31da9588b37ec28d189df2745e6e2b9
faizansafwan <faizansafwan075@gmail.com>
2024-11-01 14:29:38 +0530

    build update and delete

--- controllers/User.js
+++ controllers/User.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import User from "../models/User.js";
 
+// Create new user
 export const postUser = async (req,res) => {
 
     try {
@@ -30,6 +31,41 @@ export const postUser = async (req,res) => {
     }
 } 
 
+// update the user by the email
+export const updateUser = async (req,res) => {
+    try{
+        const { email } = req.query;
+        const { firstName, lastName, password, profilePicture } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            res.status(400).json({ success: false, message: "User not found"});
+        }
+
+        if (firstName) user.firstName = firstName;
+        if (lastName) user.lastName = lastName;
+        if (password) {
+            const saltsRound = 10;
+            const hashedPassword = await bcrypt.hash(password, saltsRound);
+
+            user.password = hashedPassword;
+        }
+
+        if (profilePicture) user.profiePicture = profilePicture;
+
+        const updateUser = await user.save();
+
+        res.status(200).json({ success: true, data: updateUser});  
+    }
+    catch (error) {
+        res.status(500).json({ success: false, message: error.message});
+    }
+    
+}
+
+
+// User login
 export const loginUser = async (req, res) => {
 
     const { email, password} = req.body;
@@ -50,6 +86,25 @@ export const loginUser = async (req, res) => {
 
 
     } 
+    catch (error) {
+        res.status(500).json({ success: false, message: error.message});
+    }
+}
+
+
+// delete user
+export const deleteUser = async (req,res) => {
+    const { id } = req.params;
+    
+    try {
+        const user = await User.findByIdAndDelete(id);
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: user});
+        }
+
+        res.status(200).json({ success: true, data: "User deleted successfully."});
+    }
     catch (error) {
         res.status(500).json({ success: false, message: error.message});
     }
