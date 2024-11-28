fe0071754d9cb2b0531c2d80435077ca35ca38b5
faizansafwan <faizansafwan075@gmail.com>
2024-11-29 00:09:45 +0530

    controller for fetch budget by email

--- controllers/Budget.js
+++ controllers/Budget.js
@@ -31,8 +31,31 @@ export const postBudget = async (req, res) => {
     }
 }
 
+export const fetchBudgetByEmail = async (req, res) => {
+    try {
+        const { email } = req.params;
+
+        // Validate email presence
+        if (!email) {
+            return res.status(400).json({ error: 'Email parameter is required.' });
+        }
+
+        const budgets = await Budget.find({email}); // Fetch all budgets
+
+        if (budgets.length === 0) {
+            return res.status(404).json({ message: 'No budgets found for the provided email.' });
+        }
+
+        res.status(200).json({ success: true, data: budgets });
+    }
+    catch (error) {
+        res.status(500).json({ message: "Error fetching budgets.", error: error.message });
+    }
+}
+
 export const fetchBudget = async (req, res) => {
     try {
+        
         const budgets = await Budget.find(); // Fetch all budgets
 
         res.status(200).json({ success: true, data: budgets });
@@ -41,3 +64,6 @@ export const fetchBudget = async (req, res) => {
         res.status(500).json({ message: "Error fetching budgets.", error: error.message });
     }
 }
+
+
+
