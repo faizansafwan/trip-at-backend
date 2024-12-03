4db8608f3919968e624de514c3532297a11591e7
faizansafwan <faizansafwan075@gmail.com>
2024-12-03 23:03:44 +0530

    controller and route for accommodation get by id

--- controllers/Accomadation.js
+++ controllers/Accomadation.js
@@ -4,14 +4,14 @@ import Accomadation from "../models/accomadation.js";
 export const postAccomadation = async (req, res) => {
 
     try {
-        const {  name, address, phone, email, description, unit, price, rating, images } = req.body;
+        const {  name, address, phone, email, description, type, unit, price, images } = req.body;
 
-        if (!name || !address || !phone || !email || !description) {
+        if (!name || !address || !phone || !email || !description || !type) {
             return res.status(400).json({ success: false, message: "All fields are required." });
         }
 
         const newAccomadation = new Accomadation({
-            name, address, phone, email, description, unit, price, rating, images
+            name, address, phone, email, description, type, unit, price, images
         });
 
         const savedAccomadation = await newAccomadation.save();
@@ -29,6 +29,30 @@ export const getAccomadation = async (req, res) => {
         const accomadation = await Accomadation.find();
 
         if(accomadation.length === 0) {
+            return res.status(400).json({success: true, message: "No Accommadation Found"});
+        }
+
+        res.status(200).json({success: true, data: accomadation});
+    }
+    catch(error) {
+        res.status(500).json({success: false, message: error.message });
+    }
+}
+
+export const getAccomadationById = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        // Id Validation
+        if (!id) {
+            return res.status(400).json({ error: 'Id parameter is required.' });
+        }
+
+        const accomadation = await Accomadation.findById(id);
+        
+
+        if(!accomadation) {
             return res.status(400).json({success: true, message: "No Accommadation Found"});
         }
 
--- routes/Accomadation.js
+++ routes/Accomadation.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getAccomadation, postAccomadation } from '../controllers/Accomadation.js';
+import { getAccomadation, getAccomadationById, postAccomadation } from '../controllers/Accomadation.js';
 import { verifyToken } from '../middlewares/auth.js';
 
 const router = express.Router();
 
 router.post('/', verifyToken, postAccomadation);
 router.get('/', getAccomadation);
+router.get('/:id', getAccomadationById);
 
 export default router;
